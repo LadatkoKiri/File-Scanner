361ab8479ddfed7accba2416703df832d47bebcf
LadatkoKiri <ladatkokiril0@gmail.com>
2026-03-15 10:41:38 +0200

    Added program for robots with files

--- src/program.py
+++ src/program.py
@@ -1,18 +1,13 @@
 import os
-
 path = input("Введіть шлях до папки: ")
-
 files = os.listdir(path)
-
 print("Файли у папці:")
 for f in files:
     print(f)
-
 print("Кількість файлів:", len(files))
-
 search = input("Введіть назву файлу для пошуку: ")
-
 if search in files:
     print("Файл знайдено")
 else:
-print("File not found")
+    print("Файл не знайдено")
+print("Програма завершена")
